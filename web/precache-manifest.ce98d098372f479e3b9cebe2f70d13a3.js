6896bcf08b0f7030bf9af6e06d971bff39e4ee6a
ivodovc <49784706+ivodovc@users.noreply.github.com>
2019-10-06 16:59:06 +0200

    Update to fit on gh pages

---
+++ web/precache-manifest.ce98d098372f479e3b9cebe2f70d13a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e5f108c08fcd041fdb9d0a3efe862e",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "8b922c589c2294dfa76b",
+    "url": "/web/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "43c7191f421f33fc83eb",
+    "url": "/web/static/js/2.dacf4daf.chunk.js"
+  },
+  {
+    "revision": "8b922c589c2294dfa76b",
+    "url": "/web/static/js/main.424533fd.chunk.js"
+  },
+  {
+    "revision": "3b0b43cb2f2d2d01e679",
+    "url": "/web/static/js/runtime-main.58cdde00.js"
+  },
+  {
+    "revision": "644fdf7737d61cf2cfa1c4148b3cb795",
+    "url": "/web/static/media/Ionicons.644fdf77.ttf"
+  }
+]);

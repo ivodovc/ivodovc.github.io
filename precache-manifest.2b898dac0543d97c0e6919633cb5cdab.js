8c036cc2f6a4360ec241d02f6a86e5c11b5efd88
ivodovc <49784706+ivodovc@users.noreply.github.com>
2019-10-06 22:53:51 +0200

    Updates

---
+++ precache-manifest.2b898dac0543d97c0e6919633cb5cdab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61419e6e56e2c40a973b8c492de1f381",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "26c46b37231415b1a8e2",
+    "url": "/web/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2e6b824f6244aa0ee05f",
+    "url": "/web/static/js/2.1a6addb9.chunk.js"
+  },
+  {
+    "revision": "26c46b37231415b1a8e2",
+    "url": "/web/static/js/main.afca444e.chunk.js"
+  },
+  {
+    "revision": "3b0b43cb2f2d2d01e679",
+    "url": "/web/static/js/runtime-main.58cdde00.js"
+  },
+  {
+    "revision": "644fdf7737d61cf2cfa1c4148b3cb795",
+    "url": "/web/static/media/Ionicons.644fdf77.ttf"
+  }
+]);

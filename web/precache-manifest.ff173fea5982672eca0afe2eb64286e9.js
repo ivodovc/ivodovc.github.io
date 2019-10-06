1f78dd8e8957a2a0d702deceec9cdf22a95190d3
ivodovc <49784706+ivodovc@users.noreply.github.com>
2019-10-06 16:52:41 +0200

    Add first web version of Hangouter

---
+++ web/precache-manifest.ff173fea5982672eca0afe2eb64286e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f02b19ea52c3e05f86cf55acc4d851",
+    "url": "/index.html"
+  },
+  {
+    "revision": "65605ee22a694d6dc750",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "43c7191f421f33fc83eb",
+    "url": "/static/js/2.dacf4daf.chunk.js"
+  },
+  {
+    "revision": "65605ee22a694d6dc750",
+    "url": "/static/js/main.3f2a01b1.chunk.js"
+  },
+  {
+    "revision": "cbb721a832bdebbc0988",
+    "url": "/static/js/runtime-main.cb1c7018.js"
+  },
+  {
+    "revision": "644fdf7737d61cf2cfa1c4148b3cb795",
+    "url": "/static/media/Ionicons.644fdf77.ttf"
+  }
+]);

cb9ac5dd5b97f1ce23efaa4a02646cf8214e081f
ivodovc <49784706+ivodovc@users.noreply.github.com>
2020-03-01 22:29:34 +0100

    Update

--- precache-manifest.00b9b8ec72d9cbb106726ae66588beb8.js
+++ precache-manifest.00b9b8ec72d9cbb106726ae66588beb8.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6a56487eefb9f2717ca48a8618624b48",
+    "revision": "15a077fdfc0c2a7ec7898e8eb7771464",
     "url": "/index.html"
   },
   {
-    "revision": "d9bd7582445178bf3a27",
+    "revision": "0ad4521b70904dd7064e",
     "url": "/static/css/main.34de6062.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.eaea0f7e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d9bd7582445178bf3a27",
-    "url": "/static/js/main.5423d540.chunk.js"
+    "revision": "0ad4521b70904dd7064e",
+    "url": "/static/js/main.0b9a0c64.chunk.js"
   },
   {
     "revision": "18a0ebf1eda9d2ba65c1",
